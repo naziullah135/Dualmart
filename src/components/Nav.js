5e54db362d7a61e5436309613433487b61f03472
unknown <mshawn192106@bscse.uiu.ac.bd>
2021-05-30 02:19:27 +0600

    add navbar & header

---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+       <div>
+           <ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">About</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Shop</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact</a>
+  </li>
+</ul>
+       </div>
+    );
+};
+
+export default Nav;
